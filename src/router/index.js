f15d3d89617b4e4e3ebff2d240e1a727a4cefd06
“thnhonnghia1” <94374646+thnhonnghia1@users.noreply.github.com>
2023-03-01 00:07:09 +0700

    Update code

--- src/router/index.js
+++ src/router/index.js
@@ -7,11 +7,11 @@ import English from "../components/layouts/English.vue";
 import InformationTechnology from "../components/layouts/InformationTechnology.vue";
 
 const routes = [
-  { path: "/", name: "Trang chủ", component: Home, isLoad: true },
-  {path: "/tieng-viet", name: "Tiếng Việt", component: Vietnamese, isLoad: true},
-  { path: "/toan-hoc", name: "Toán Học", component: Math, isLoad: true },
-  { path: "/tieng-anh", name: "Tiếng Anh", component: English, isLoad: true },
-  { path: "/tin-hoc", name: "Tin học", component: InformationTechnology, isLoad: true },
+  { path: "/smart-monkeys", name: "Trang chủ", component: Home, isLoad: true },
+  {path: "/smart-monkeys/tieng-viet", name: "Tiếng Việt", component: Vietnamese, isLoad: true},
+  { path: "/smart-monkeys/toan-hoc", name: "Toán Học", component: Math, isLoad: true },
+  { path: "/smart-monkeys/tieng-anh", name: "Tiếng Anh", component: English, isLoad: true },
+  { path: "/smart-monkeys/tin-hoc", name: "Tin học", component: InformationTechnology, isLoad: true },
 ];
 
 const router = new createRouter({
